51b1f7064c74e6e058b6fe7e74ec5ed44d4306cb
Adam Jansch <adam@adamjansch.co.uk>
2021-01-17 09:04:31 +0000

    Initial Commit

---
+++ AudioKit SysEx crash/AudioKit_SysEx_crashApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKit_SysEx_crashApp.swift
+//  AudioKit SysEx crash
+//
+//  Created by Adam Jansch on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AudioKit_SysEx_crashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
