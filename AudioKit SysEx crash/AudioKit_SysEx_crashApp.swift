119625e9ab666768f9d6679f0fecf364c7243ba8
Adam Jansch <adam@adamjansch.co.uk>
2021-01-17 09:23:20 +0000

    Added code for minimal reproducible situation.

--- AudioKit SysEx crash/AudioKit_SysEx_crashApp.swift
+++ AudioKit SysEx crash/AudioKit_SysEx_crashApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AudioKit
+import CoreMIDI
 
 @main
 struct AudioKit_SysEx_crashApp: App {
@@ -14,4 +16,65 @@ struct AudioKit_SysEx_crashApp: App {
             ContentView()
         }
     }
+	
+	
+	// MARK: - METHODS
+	// MARK: Initializers
+	init() {
+		let midi = MIDI.sharedInstance
+		
+		print("MIDI Sources:      \(midi.inputInfos)")
+		print("MIDI Destinations: \(midi.destinationInfos)")
+		
+		midi.clearListeners()
+		midi.addListener(self)
+		
+		midi.openInput(uid: 0)
+	}
+}
+
+extension AudioKit_SysEx_crashApp: MIDIListener {
+	func receivedMIDISystemCommand(_ data: [MIDIByte], portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		print("SYSEX received: \(data)")
+	}
+	
+	func receivedMIDINoteOn(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDINoteOff(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDIController(_ controller: MIDIByte, value: MIDIByte, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDIAftertouch(noteNumber: MIDINoteNumber, pressure: MIDIByte, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDIAftertouch(_ pressure: MIDIByte, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDIPitchWheel(_ pitchWheelValue: MIDIWord, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDIProgramChange(_ program: MIDIByte, channel: MIDIChannel, portID: MIDIUniqueID?, offset: MIDITimeStamp) {
+		// Not used
+	}
+	
+	func receivedMIDISetupChange() {
+		// Not used
+	}
+	
+	func receivedMIDIPropertyChange(propertyChangeInfo: MIDIObjectPropertyChangeNotification) {
+		// Not used
+	}
+	
+	func receivedMIDINotification(notification: MIDINotification) {
+		// Not used
+	}
 }
